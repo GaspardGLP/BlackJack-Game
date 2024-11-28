e30b59b5b68c3a71488b0c4f57a7c4a52f7229df
GLPgaspard <gaspard.grignet-le-perron@epitech.eu>
2024-11-28 23:24:13 +0100

    [ADD] Add the main

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
 #include "blackjack.h"
+
+int main() {
+    srand(time(NULL));
+
+    Deck *deck = NULL;
+    Player player = {NULL, 0, 0};
+    Player dealer = {NULL, 0, 0};
+
+    initialize_deck(&deck);
+
+    printf("Initializing deck\n");
+
+    deal_card(&deck, &player);
+    deal_card(&deck, &player);
+    deal_card(&deck, &dealer);
+    deal_card(&deck, &dealer);
+
+    printf("\nPlayer's hand:\n");
+    display_player_hand(&player);
+
+    printf("\nDealer's hand:\n");
+    display_player_hand(&dealer);
+
+    free_deck(deck);
+
+    return 0;
+}
